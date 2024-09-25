908f9482736b0e0fcf32a13bb3dd1dd6b9c9750c
Taranpreet Singh <99615766+TaranPannu@users.noreply.github.com>
2024-09-25 07:39:42 -0230

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,3 +1,25 @@
-public enum Player {
-    player1, player2, player3, player4
+// public enum Player {
+//     player1, player2, player3, player4
+// }
+public abstract class Player {
+    // Common fields
+    protected String name;
+    protected int score;
+
+    public Player(String name) {
+        this.name = name;
+        this.score = 0;
+    }
+
+    // Abstract method
+    public abstract void makeMove();
+
+    // Common methods
+    public String getName() {
+        return name;
+    }
+
+    public int getScore() {
+        return score;
+    }
 }
